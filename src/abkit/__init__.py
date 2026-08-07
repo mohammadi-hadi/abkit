7bd1c22d1a66ef0860aab232df278cdbcd64eadc
Hadi Mohammadi <50410241+mohammadi-hadi@users.noreply.github.com>
2026-08-07 22:17:49 +0200

    Release 0.1.1

--- src/abkit/__init__.py
+++ src/abkit/__init__.py
@@ -5,7 +5,7 @@ from .io import dump_design, dump_units, load_design, load_units
 from .result import AuditResult, ProbeResult, SkippedProbe
 from .schema import Design, Look, Unit
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "AuditResult",
